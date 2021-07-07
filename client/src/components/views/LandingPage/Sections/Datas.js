e2b6f15b38032d5a832d61a6faf12da5f1f52173
kkong101 <aflhzv@gmail.com>
2021-07-07 23:58:56 +0900

    Myinfo page

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,88 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$200 to $249",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "$250 to $279",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "$280 to $299",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $300",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const goods = [
+    {
+        "_id": 1,
+        "name": "Phone"
+    },
+    {
+        "_id": 2,
+        "name": "Monitor"
+    },
+    {
+        "_id": 3,
+        "name": "Computer"
+    },
+    {
+        "_id": 4,
+        "name": "Graphic Card"
+    },
+    {
+        "_id": 5,
+        "name": "Camera"
+    },
+    {
+        "_id": 6,
+        "name": "Speaker"
+    },
+    {
+        "_id": 7,
+        "name": "keyboard"
+    },
+    {
+        "_id": 8,
+        "name": "Mouse"
+    }
+]
+
+const goodsList = [
+    { key: 1, value: "Phone" },
+    { key: 2, value: "Monitor" },
+    { key: 3, value: "Computer" },
+    { key: 4, value: "Graphic Card" },
+    { key: 5, value: "Camera" },
+    { key: 6, value: "Speaker" },
+    { key: 7, value: "keyboard" },
+    { key: 8, value: "Mouse"}
+]
+
+
+export {
+    price,
+    goods,
+    goodsList
+}
---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    mongoURI : "mongodb+srv://aflhzv:1234@cluster.rpjbh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
+
